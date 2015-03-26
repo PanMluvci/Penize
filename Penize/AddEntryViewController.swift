bb8783f5951d016b5111fa8bc336b9ee156e255d
PanMluvci <darkkai@seznam.cz>
2015-03-26 19:01:50 +0100

    Added graphic buttons, and array with values.

--- Penize/AddEntryViewController.swift
+++ Penize/AddEntryViewController.swift
@@ -45,4 +45,17 @@ class AddEntryViewController: UIViewController {
         
     }
     
+    var colors = ["Red","Yellow","Green","Blue"]
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return colors[row]
+    }
+    
 }
