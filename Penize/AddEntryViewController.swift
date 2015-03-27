f9b3e64103fb6831358b107fdcdbc0e319924860
PanMluvci <darkkai@seznam.cz>
2015-03-27 16:06:33 +0100

    Save Info  while saving.

--- Penize/AddEntryViewController.swift
+++ Penize/AddEntryViewController.swift
@@ -14,12 +14,17 @@ class AddEntryViewController: UIViewController {
     @IBOutlet weak var costTxtField: UITextField!
     @IBOutlet weak var noteTxtField: UITextField!
     @IBOutlet weak var infoField: UILabel!
+    @IBOutlet weak var checkmarkSave: UIImageView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor(red: 117/255, green: 209/255, blue: 255/255, alpha: 1.0)
-        infoField.hidden = true
+        //infoField.hidden = true
+        self.infoField.alpha = 0
+        self.infoField.font = UIFont(name: "Zapfino", size: 20)
+        self.infoField.text = "Activity Saved"
     }
     
     override func didReceiveMemoryWarning() {
@@ -35,12 +40,30 @@ class AddEntryViewController: UIViewController {
         var newUser: AnyObject = NSEntityDescription.insertNewObjectForEntityForName("Acc", inManagedObjectContext: context)
         newUser.setValue(costTxtField.text, forKey: "user")
         newUser.setValue(noteTxtField.text, forKey: "pass")
-        
+        animateCountdownLabel()
         context.save(nil)
         println("Saved")
-        infoField.hidden = false
         
+       // infoPanelPrompt()
         
+    }
+   
+    
+    func animateCountdownLabel()
+    {
+        UIView.animateWithDuration(1.5, animations: { () -> Void in
+            self.checkmarkSave.hidden = true
+            self.infoField.alpha = 1.0
+            
+            }) { (Bool) -> Void in
+                UIView.animateWithDuration(1.5, animations: { () -> Void in
+                    
+                    self.infoField.alpha = 0.0
+                    
+                    }, completion: { (Bool) -> Void in
+                      self.checkmarkSave.hidden = false
+                })
+        }
     }
     
     @IBAction func backHome(sender: AnyObject) {
